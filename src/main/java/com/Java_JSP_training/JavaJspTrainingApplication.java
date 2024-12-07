29f96e30f6b4bef875733b03c760c5b0d6e59c93
Matheus_Freitas0 <moncaomatheus@gmail.com>
2024-12-06 23:16:22 -0300

    First commit

---
+++ src/main/java/com/Java_JSP_training/JavaJspTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.Java_JSP_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJspTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJspTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java_JSP_training/JavaJspTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java_JSP_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJspTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
